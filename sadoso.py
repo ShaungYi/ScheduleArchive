51aa7d0273c7b3a6c969c5ad1efe9668848cb72a
ShaungYi <seongha003@gmail.com>
2021-10-05 18:04:51 +0900

    fxml fix + sadoso

---
+++ sadoso.py
@@ -0,0 +1,116 @@
+import sqlite3
+import os
+
+
+class ScheduleArchiveDBOptimizer:
+
+    def __init__(self):
+        self.destination = "C:/Users/Joonius/Desktop/JavaProjects/IdeaProjects/ScheduleArchiveMacroUpdateOptimized/Databases/"
+        self.source = "C:/Users/Joonius/Desktop/JavaProjects/IdeaProjects/ScheduleArchiveMacroUpdateOptimized/Databases/archive.db"
+        self.dbName = "archive2.db"
+
+        os.chdir(self.destination)
+
+        self.oldDB = sqlite3.connect(self.source)
+        self.optimizedDB = sqlite3.connect(self.dbName)
+
+        self.oldDBCursor = self.oldDB.cursor()
+        self.optimizedDBCursor = self.optimizedDB.cursor()
+
+
+    def fillActivitiesTable(self):
+        self.oldDBCursor.execute(
+            "SELECT DISTINCT activityID, name, category, frequency "
+            "FROM activities "
+            "JOIN frequencies "
+            "USING (activityID)"
+        )
+        activitiesTableContent = self.oldDBCursor.fetchall()
+        self.optimizedDBCursor.executemany("INSERT INTO "
+                                           "activities "
+                                           "VALUES (?, ?, ?, ?)",
+                                           activitiesTableContent
+                                           )
+
+
+
+    def fillEventsTable(self):
+        self.oldDBCursor.execute("SELECT * FROM events")
+        eventsTableContent = self.oldDBCursor.fetchall()
+        self.optimizedDBCursor.executemany(
+                                           "INSERT INTO "
+                                           "events "
+                                           "("
+                                           "activityID, "
+                                           "startTime, "
+                                           "endTime, "
+                                           "date"
+                                           ") "
+                                           "VALUES (?, ?, ?, ?)",
+                                           eventsTableContent
+                                           )
+
+
+    def createDB(self):
+        print("[+] Creating new DB...")
+
+        self.optimizedDBCursor.execute(
+            "CREATE TABLE "
+            "IF NOT EXISTS "
+            "activities "
+            "("
+            "activityID INTEGER, "
+            "name TEXT, "
+            "category TEXT, "
+            "frequency INTEGER"
+            ")"
+        )
+
+        self.optimizedDBCursor.execute(
+            "CREATE TABLE "
+            "IF NOT EXISTS "
+            "events "
+            "("
+            "activityID INTEGER, "
+            "description TEXT, "
+            "startTime, "
+            "endTime INTEGER, "
+            "date TEXT"
+            ")"
+        )
+
+        self.optimizedDBCursor.execute("DELETE FROM activities")
+        self.optimizedDBCursor.execute("DELETE FROM events")
+
+        # reading databases
+
+        print("[+] Reading file %s..." % self.source)
+
+        # giving activities id and storing them in the activities table
+
+        print("[+] Moving data to 'activities' table...")
+        self.fillActivitiesTable()
+
+        # matching id with time and storing them in the time table
+
+        print("[+] Moving data to 'events' table...")
+        self.fillEventsTable()
+
+        # saving and closing databases
+
+        print("[+] Saving databases...")
+
+        self.optimizedDB.commit()
+        self.optimizedDB.close()
+
+        self.oldDB.close()
+
+        print("[+] Created file " + self.destination + self.dbName)
+
+
+    def run(self):
+        self.createDB()
+
+
+optimizer = ScheduleArchiveDBOptimizer()
+optimizer.run()
